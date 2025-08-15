32cd98d9d78f1607838c3d7dece5ca260fbc77db
fmdirnbeck-ctrl <fmdirnbeck@gmail.com>
2025-08-15 16:01:01 -0400

    Create index.js

---
+++ Src/index.js
@@ -0,0 +1,6 @@
+javascriptimport React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProtectedGoodLoopPlatform from './ProtectedGoodLoopPlatform';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ProtectedGoodLoopPlatform />);
